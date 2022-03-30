fa9c1120e5f6176ae70e8dcc411429e52d4bb21d
251205668 <y251205668@163.com>
2022-03-30 08:36:15 +0000

    Update vitest-vue-starter 6d78406a68670d0b0e0331488c20464c7c7de700

---
+++ assets/404.26f43e2f.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.c9c74659.js";import{o,e as r}from"./vendor.5707a39e.js";const t={};function c(n,_){return o(),r("div",null," Not Found ")}var f=e(t,[["render",c]]);export{f as default};
--- assets/404.80de07a7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.328b68f8.js";import{o,e as r}from"./vendor.74d1edaf.js";const t={};function c(n,_){return o(),r("div",null," Not Found ")}var f=e(t,[["render",c]]);export{f as default};
