6206026be5eb131e707341be4fe4268716450e8d
251205668 <y251205668@163.com>
2022-03-30 08:24:50 +0000

    Update vitest-vue-starter 947bf6e8686bb703de1044be560fd1ec316770ce

---
+++ assets/404.80de07a7.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.328b68f8.js";import{o,e as r}from"./vendor.74d1edaf.js";const t={};function c(n,_){return o(),r("div",null," Not Found ")}var f=e(t,[["render",c]]);export{f as default};
