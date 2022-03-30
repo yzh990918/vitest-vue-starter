4a46f114954dd325b33890f04edfdcca86549bb1
251205668 <y251205668@163.com>
2022-03-30 09:28:13 +0000

    Update vitest-vue-starter fe1c6123ab714c473ffeb802a3cc0ffe374e791a

---
+++ assets/404.0366d195.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.f7d125e1.js";import{o,e as r}from"./vendor.fc4f3fbb.js";const t={};function c(n,_){return o(),r("div",null," Not Found ")}var f=e(t,[["render",c]]);export{f as default};
--- assets/404.26f43e2f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.c9c74659.js";import{o,e as r}from"./vendor.5707a39e.js";const t={};function c(n,_){return o(),r("div",null," Not Found ")}var f=e(t,[["render",c]]);export{f as default};
