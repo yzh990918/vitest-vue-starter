dfd26746c0fdeeb774e4ffcb88530f2beefd67f6
mohen <251205668@qq.com>
2022-08-25 17:50:48 +0800

    chore: update tsconfig.json

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-    "extends": ["@commitlint/config-conventional"],
-    "rules": {
-        "type-enum": [
-            2,
-            "always",
-            [
-                "build",
-                "chore",
-                "ci",
-                "docs",
-                "feat",
-                "fix",
-                "perf",
-                "refactor",
-                "revert",
-                "style",
-                "test",
-                "record"
-            ],
-        ],
-        "type-empty": [0],
-        "type-case": [0],
-        "scope-empty": [0],
-        "header-max-length": [0, 'always', 72]
-    },
-};
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'record',
+      ],
+    ],
+    'type-empty': [0],
+    'type-case': [0],
+    'scope-empty': [0],
+    'header-max-length': [0, 'always', 72],
+  },
+}
